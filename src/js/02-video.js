fa81a69d6d164717ba0842c0e65a04b6d392eab9
Dikhtiar-Tania96 <chygynovatania96@gmail.com>
2024-04-04 14:42:49 +0300

    second task commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function fillLocalStorage(data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+}
+
+player.on('timeupdate', throttle(fillLocalStorage, 1000));
+
+
+const currentTime = localStorage.getItem(STORAGE_KEY);
+
+player.setCurrentTime(currentTime || 0);
+
 
